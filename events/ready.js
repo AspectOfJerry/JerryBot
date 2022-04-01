bfa8e8c9beba9db4818ce09a59379daaf34c341f
AspectOfJerry <jerryfu_mc@outlook.com>
2022-03-31 21:00:31 -0400

    April fools

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,8 @@
 module.exports = (client) => {
-    //client.user.setActivity("", {type: ""})
+    //client.user.setActivity("String", {type: "PLAYING"})
+
+    client.user.setActivity("April Fools Simulator", {type: "PLAYING"})
+    /* April Fools
+    client.user.setActivity("April Fools Simulator", {type: "PLAYING"})
+    */
 }
