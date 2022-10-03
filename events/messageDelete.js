c0508fd162daeebf4ce37ec1ab0b6b8b307348df
AspectOfJerry <jerryfu_mc@outlook.com>
2022-10-03 16:06:24 -0400

    new event files

---
+++ events/messageDelete.js
@@ -0,0 +1,10 @@
+const Sleep = require('../modules/sleep'); // delayInMilliseconds
+const Log = require('../modules/logger'); // DEBUG, ERROR, FATAL, INFO, LOG, WARN; │, ─, ├─, └─
+
+module.exports = {
+    name: "messageDelete",
+    once: false, // Whether or not this even should only be triggered once
+    async execute(err) {
+        await Log('append', 'EVENT_NAME', "", ''); // Logs
+    }
+};
