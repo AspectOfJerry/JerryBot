6532ddf603a5a73e9ecb8b5c034494a08c6bfdb4
Jerry <jerryfu_mc@outlook.com>
2022-10-24 19:43:16 -0400

    started to work on #50

---
+++ commands/dev/dev_subcommands/dev_stop.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const {Client, Collection, Intents, MessageActionRow, MessageButton, MessageEmbed, ModalBuilder} = require('discord.js');
+const {joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus, getVoiceConnection} = require('@discordjs/voice');
+
+const Sleep = require('../../../modules/sleep'); // delayInMilliseconds
+const Log = require('../../../modules/logger'); // DEBUG, ERROR, FATAL, INFO, LOG, WARN; │, ─, ├─, └─
+
+module.exports = async function (client, interaction) {
+    await Log('append', interaction.guild.id, `└─'${interaction.user.tag}' executed '/dev stop'.`, 'INFO'); // Logs
+
+    // Whitelist
+
+    // Declaring variables
+
+    // Checks
+
+    // Main
+
+};
