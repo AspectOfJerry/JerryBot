b0816f3aa2e286c7ac787693ec8fd9a9c1184a71
Jerry <jerryfu_mc@outlook.com>
2022-12-09 16:46:54 -0500

    fixed bug

--- commands/other/quiz/quiz.js
+++ commands/other/quiz/quiz.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const {Client, Collection, Intents, MessageActionRow, MessageButton, MessageEmbed, Modal, TextInputComponent} = require('discord.js');
 const {SlashCommandBuilder} = require("@discordjs/builders");
 
-const {Log, Sleep} = require('../../modules/JerryUtils');
+const {Log, Sleep} = require('../../../modules/JerryUtils');
+
+const quiz_1_name = "esp_translate"
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,40 +12,38 @@ module.exports = {
         .setDescription("Custom quiz commands")
         .addSubcommand(subcommand =>
             subcommand
-                .setName('quiz_define')
-                .setDescription("")
-                .addStringOption((options) =>
-                    options
-                        .setName('SUBCMD_OPTION_NAME')
-                        .setDescription("[REQUIRED/OPTIONAL] SUBCMD_OPTION_DESCRIPTION")
-                        .setRequired(true / false)))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('SUBCMD_NAME')
-                .setDescription("SUBCMD_DESCRIPTION")
-                .addStringOption((options) =>
-                    options
-                        .setName('SUBCMD_OPTION_NAME')
-                        .setDescription("[REQUIRED/OPTIONAL] SUBCMD_OPTION_DESCRIPTION")
-                        .setRequired(true / false))),
+                .setName(`${quiz_1_name}`)
+                .setDescription("Quiz on word translation"))
+    // .addSubcommand(subcommand =>
+    //     subcommand
+    //         .setName('SUBCMD_NAME')
+    //         .setDescription("SUBCMD_DESCRIPTION")
+    //         .addStringOption((options) =>
+    //             options
+    //                 .setName('SUBCMD_OPTION_NAME')
+    //                 .setDescription("[REQUIRED/OPTIONAL] SUBCMD_OPTION_DESCRIPTION")
+    //                 .setRequired(true / false)))
+    ,
     async execute(client, interaction) {
-        await Log('append', interaction.guild.id, `'${interaction.user.tag}' executed '/CMD_NAME [...]'.`, 'INFO'); // Logs
+        await Log('append', interaction.guild.id, `'${interaction.user.tag}' executed '/quiz [...]'.`, 'INFO'); // Logs
 
         // Declaring variables
         const subcommand = interaction.options.getSubcommand();
 
         // Checks
+        await interaction.reply('This command is currently under development and will be available soon.');
+        return;
 
         // Main
         switch(subcommand) {
             case 'SUBCMD_NAME': {
-                await Log('append', "subcmd_hdlr", `├─'${interaction.user.tag}' executed '/CMD_NAME SUBCMD_NAME'.`, 'INFO'); // Logs
+                await Log('append', "subcmd_hdlr", `├─'${interaction.user.tag}' executed '/quiz ${quiz_1_name}'.`, 'INFO'); // Logs
 
                 // Prep
 
                 // Calling the subcommand file
                 await Log('append', "subcmd_hdlr", `├─Handing controls to subcommand file...`, 'DEBUG'); // Logs
-                require('./FILE.subcmd.js')(client, interaction);
+                require(`./${quiz_1_name}.subcmd.js`)(client, interaction);
             }
                 break;
             case 'SUBCMD_NAME': {
