46b1d0fffc7a78ebc99faa4b7bb79ccfaf39f489
Jerry <jerryfu_mc@outlook.com>
2023-10-27 19:43:14 -0400

    patch

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,21 +7,6 @@ export default {
     name: "guildMemberAdd",
     once: false,
     async execute(member) {
-        if(member.guild.id === "631939549332897842") { // devServer
-            const channel = member.guild.channels.resolve("857850833982193665");
-
-            const join_message = new MessageEmbed()
-                .setColor("GREEN")
-                .setThumbnail(`${member.user.displayAvatarURL({dynamic: true, size: 256})}`)
-                .setTitle("User join")
-                .setDescription(`<@${member.user.id}> joined the guild!`)
-                .setTimestamp();
-
-            channel.send({embeds: [join_message]});
-
-            member.roles.add("829896942208155678");
-        }
-
         logger.append("info", "0x474D41", `[GMA] '@${member.user.tag}' joined guild "${member.guild.id}"!`);
     }
 };
